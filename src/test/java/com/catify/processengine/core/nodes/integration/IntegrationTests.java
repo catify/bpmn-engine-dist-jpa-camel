820efcfc4c9f53e43768587832852b63f36fced3
chris-koester <koester.christopher@googlemail.com>
2013-02-15 14:28:05 +0100

    refactored integration tests, added test cases for boundary events

--- src/test/java/com/catify/processengine/core/nodes/integration/IntegrationTests.java
+++ src/test/java/com/catify/processengine/core/nodes/integration/IntegrationTests.java
@@ -63,30 +63,7 @@ import com.catify.processengine.management.XmlJaxbTransformer;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:META-INF/spring/spring-context.xml" })
 //@Transactional
-public class IntegrationTests {
-	
-	static final Logger LOG = LoggerFactory
-			.getLogger(IntegrationTests.class);
-	
-	@Autowired
-    private ApplicationContext ctx;
-	
-	@Autowired 
-	private Neo4jTemplate neo4jTemplate;
-	
-	@Autowired
-	private FlowNodeInstanceRepositoryService flowNodeInstanceRepo;
-	
-	@Autowired
-	private ProcessNodeRepositoryService processNodeRepo;
-
-    private final String client = "Client";
-    private final String startEvent = "startEvent1";
-//    private final String catchEvent = "catchEvent1";
-    private final String defaultInstanceId = "42";
-    
-    private ProcessManagementService pm = new ProcessManagementServiceImpl();
-    private XmlJaxbTransformer xmlJaxbTransformer = new XmlJaxbTransformer();
+public class IntegrationTests extends IntegrationTestBase {
 	
 	@Test
 	public void testprocessTimerCatchDuration() throws IOException, JAXBException, InterruptedException {
@@ -122,229 +99,111 @@ public class IntegrationTests {
 		// wait for the process instance to start up
 		Thread.sleep(3000);
 		
-	    // trigger the waiting catch event
+	    // trigger the waiting boundary event
 	    pm.sendTriggerMessage(client, process, "_8", new TriggerMessage(defaultInstanceId, null));
-		
 		Thread.sleep(3000);
 		
+		// trigger the waiting receiving activity
 		pm.sendTriggerMessage(client, process, "_5", new TriggerMessage(defaultInstanceId, null));
-		
 		Thread.sleep(3000);
 		
 		// check results
 		Assert.assertEquals(12, getFlowNodeCount(process));
 		Assert.assertEquals(6, getFlowNodeInstanceCount(process));
-		
 	    Assert.assertTrue(checkFlowNodeInstanceState(NodeInstaceStates.PASSED_STATE, process, defaultInstanceId));
-	    checkNodeInstance(process, "_5", NodeInstaceStates.PASSED_STATE);
+	}
+	
+	@Test
+	public void testprocessBoundaryNoninterruptingActivatedMultiple() throws IOException, JAXBException, InterruptedException {
+
+		TProcess process = startProcess("testprocess_boundary_noninterrupting_activated_multiple_messageIntegration_camel.bpmn", 4000);
+		pm.createProcessInstance(client, process, startEvent, new TriggerMessage(defaultInstanceId, null));
+
+		// wait for the process instance to start up
+		Thread.sleep(3000);
+		
+	    // trigger the first waiting boundary event
+	    pm.sendTriggerMessage(client, process, "_8", new TriggerMessage(defaultInstanceId, null));
+		
+	    // trigger the second waiting boundary event
+	    pm.sendTriggerMessage(client, process, "_2", new TriggerMessage(defaultInstanceId, null));
+		Thread.sleep(3000);
+		
+		// trigger the waiting receiving activity
+		pm.sendTriggerMessage(client, process, "_5", new TriggerMessage(defaultInstanceId, null));
+		Thread.sleep(3000);
+		
+		// check results
+		Assert.assertEquals(14, getFlowNodeCount(process));
+		Assert.assertEquals(7, getFlowNodeInstanceCount(process));
+	    Assert.assertTrue(checkFlowNodeInstanceState(NodeInstaceStates.PASSED_STATE, process, defaultInstanceId));
+	}
+	
+	@Test
+	public void testprocessBoundaryInterruptingActivatedMultiple() throws IOException, JAXBException, InterruptedException {
+
+		TProcess process = startProcess("testprocess_boundary_interrupting_activated_multiple_messageIntegration_camel.bpmn", 4000);
+		pm.createProcessInstance(client, process, startEvent, new TriggerMessage(defaultInstanceId, null));
+
+		// wait for the process instance to start up
+		Thread.sleep(3000);
+		
+	    // trigger the first waiting boundary event
+	    pm.sendTriggerMessage(client, process, "_8", new TriggerMessage(defaultInstanceId, null));
+	    Thread.sleep(3000);
+		
+	    // trigger the second waiting boundary event (should not have an effect, because it will have been deactivated)
+	    pm.sendTriggerMessage(client, process, "_2", new TriggerMessage(defaultInstanceId, null));
+		
+		// trigger the waiting receiving activity (should not have an effect, because it will have been deactivated)
+		pm.sendTriggerMessage(client, process, "_5", new TriggerMessage(defaultInstanceId, null));
+		Thread.sleep(3000);
+		
+		// check results
+		Assert.assertEquals(14, getFlowNodeCount(process));
+		Assert.assertEquals(7, getFlowNodeInstanceCount(process));
+	    Assert.assertFalse(checkFlowNodeInstanceState(NodeInstaceStates.PASSED_STATE, process, defaultInstanceId));
+	    
+	    checkNodeInstance(process, "_2", NodeInstaceStates.DEACTIVATED_STATE);
+	    checkNodeInstance(process, "_5", NodeInstaceStates.DEACTIVATED_STATE);
 	    checkNodeInstance(process, "_8", NodeInstaceStates.PASSED_STATE);
 	    checkNodeInstance(process, "_9", NodeInstaceStates.PASSED_STATE);
 	    checkNodeInstance(process, "_10", NodeInstaceStates.PASSED_STATE);
 	}
 	
+	@Test
+	public void testprocessBoundaryInterruptingTimerActivatedSubprocess() throws IOException, JAXBException, InterruptedException {
+
+		TProcess process = startProcess("testprocess_boundary_interrupting_timer_activated_subprocess.bpmn", 4000);
+		pm.createProcessInstance(client, process, startEvent, new TriggerMessage(defaultInstanceId, null));
+
+		// wait for the process instance to start up
+		Thread.sleep(3000);
+		
+	    // do not trigger the waiting catch event in the sub process so that the boundary timer event fires after 5 secs
+		Thread.sleep(9000);
+		
+		// check results
+		Assert.assertEquals(10, getFlowNodeCount(process));
+		Assert.assertEquals(8, getFlowNodeInstanceCount(process));
+	    Assert.assertFalse(checkFlowNodeInstanceState(NodeInstaceStates.PASSED_STATE, process, defaultInstanceId));
+	    
+	    // check sub process and end events
+	    checkNodeInstance(process, "_8", NodeInstaceStates.PASSED_STATE);
+	    checkNodeInstance(process, "_4", NodeInstaceStates.DEACTIVATED_STATE);
+	    checkNodeInstance(process, "_3", NodeInstaceStates.INACTIVE_STATE);
+	    
+	    // check sub process embedded events
+	    checkNodeInstance(process, "_10", NodeInstaceStates.PASSED_STATE);
+	    checkNodeInstance(process, "_11", NodeInstaceStates.DEACTIVATED_STATE);
+	    checkNodeInstance(process, "_12", NodeInstaceStates.INACTIVE_STATE);
+	    
+	    Thread.sleep(1000);
+	}
 	
 	@Test
 	public void testprocessThrowMessageIntegration() throws IOException, JAXBException, InterruptedException {
 		TProcess process = simpleProcessTest("testprocess_throw_camel_messageIntegration.bpmn", 3000, 5000, 6, 3);
 	    Assert.assertTrue(checkFlowNodeInstanceState(NodeInstaceStates.PASSED_STATE, process, defaultInstanceId));
-	}
-	
-	/**
-	 * Helper method to test standard scenarios.
-	 *
-	 * @param fileName name of the file without path and slash (must be in /src/test/resources/data/)
-	 * @param firstSleep milliseconds of the first sleep
-	 * @param secondSleep milliseconds of the first sleep
-	 * @param awaitedFlowNodeCount awaited number of flow nodes
-	 * @param awaitedInstanceNodeCount awaited number of flow node instances
-	 * @return the jaxb process
-	 * @throws FileNotFoundException the file not found exception
-	 * @throws JAXBException the jAXB exception
-	 * @throws InterruptedException the interrupted exception
-	 */
-	private TProcess simpleProcessTest(String fileName, int firstSleep, int secondSleep, int awaitedFlowNodeCount, int awaitedInstanceNodeCount) 
-			throws FileNotFoundException, JAXBException, InterruptedException {
-		TProcess process = startProcess(fileName, firstSleep);
-		pm.createProcessInstance(client, process, startEvent, new TriggerMessage(defaultInstanceId, null));
-
-		// wait for the process instance to start up
-		Thread.sleep(secondSleep);
-
-		// check results
-		Assert.assertEquals(awaitedFlowNodeCount, getFlowNodeCount(process));
-		Assert.assertEquals(awaitedInstanceNodeCount, getFlowNodeInstanceCount(process));
-		
-		return process;
-	}
-	
-//	/**
-//	 * Helper method to test standard scenarios with a node to trigger.
-//	 *
-//	 * @param fileName name of the file without path and slash (must be in /src/test/resources/data/)
-//	 * @param flowNodeIdToTrigger the flow node id to trigger
-//	 * @param firstSleep milliseconds of the first sleep
-//	 * @param secondSleep milliseconds of the first sleep
-//	 * @param thirdSleep milliseconds of the third sleep
-//	 * @param awaitedFlowNodeCount awaited number of flow nodes
-//	 * @param awaitedInstanceNodeCount awaited number of flow node instances
-//	 * @return the jaxb process
-//	 * @throws FileNotFoundException the file not found exception
-//	 * @throws JAXBException the jAXB exception
-//	 * @throws InterruptedException the interrupted exception
-//	 */
-//	private TProcess simpleProcessTestWithTrigger(String fileName, String flowNodeIdToTrigger, int firstSleep, int secondSleep, int thirdSleep, int awaitedFlowNodeCount, int awaitedInstanceNodeCount) 
-//			throws FileNotFoundException, JAXBException, InterruptedException {
-//		TProcess process = startProcess(fileName, firstSleep);
-//		pm.createProcessInstance(client, process, startEvent, new TriggerMessage(defaultInstanceId, null));
-//
-//		// wait for the process instance to start up
-//		Thread.sleep(secondSleep);
-//		
-//	    // trigger the waiting catch event
-//	    pm.sendTriggerMessage(client, process, flowNodeIdToTrigger, new TriggerMessage(defaultInstanceId, null));
-//	    
-//	    // wait for the process instance to end
-//	    Thread.sleep(thirdSleep);
-//
-//		// check results
-//		Assert.assertEquals(awaitedFlowNodeCount, getFlowNodeCount(process));
-//		Assert.assertEquals(awaitedInstanceNodeCount, getFlowNodeInstanceCount(process));
-//		
-//		return process;
-//	}
-	
-	/**
-	 * Start a process.
-	 *
-	 * @param fileName name of the file without path and slash (must be in /src/test/resources/data/)
-	 * @param firstSleep milliseconds of the first sleep
-	 * @return the jaxb process
-	 * @throws FileNotFoundException the file not found exception
-	 * @throws JAXBException the jAXB exception
-	 * @throws InterruptedException the interrupted exception
-	 */
-	private TProcess startProcess(String fileName, int firstSleep)
-			throws FileNotFoundException, JAXBException, InterruptedException {
-		File processDefinition = new File(getClass().getResource("/data/" + fileName).getFile());
-		Assert.assertTrue(processDefinition.exists());
-
-		pm.startProcessFromDefinitionFile(client, processDefinition);
-
-		// wait for the process to start up
-		Thread.sleep(firstSleep);
-
-		List<TProcess> processes = xmlJaxbTransformer.getTProcessesFromBpmnXml(processDefinition);
-		assertNotNull(processes);
-		return processes.get(0);
-	}
-	
-	/**
-	 * Gets the flow node count. Note that a process always has running and archived flow nodes.
-	 *
-	 * @return the flow node count
-	 */
-	long getFlowNodeCount(TProcess process) {
-		String processId = IdService.getUniqueProcessId(client, process);
-		ProcessNode processNode = processNodeRepo.findByUniqueProcessId(processId);
-		ProcessNode archivedProcessNode = processNodeRepo.findArchivedByRunningUniqueProcessId(processId);
-		
-		return processNode.getFlowNodes().size() + archivedProcessNode.getFlowNodes().size();
-	}
-	
-	/**
-	 * Gets the flow node instance count.
-	 *
-	 * @return the flow node instance count
-	 */
-	long getFlowNodeInstanceCount(TProcess process) {		
-		return this.getFlowNodeInstanceCount(process, defaultInstanceId);
-	}
-	
-	/**
-	 * Gets the flow node instance count.
-	 * 
-	 * @param process {@link TProcess} process object
-	 * @param instanceId instance id as {@link String}
-	 * @return
-	 */
-	long getFlowNodeInstanceCount(TProcess process, String instanceId) {
-		String processId = IdService.getUniqueProcessId(client, process);
-		Set<FlowNodeInstance> fni = flowNodeInstanceRepo.findAllFlowNodeInstances(processId, instanceId);
-		
-		return Iterables.count(fni);
-	}
-
-	/**
-	 * Check flow node instance state.
-	 *
-	 * @param state the state
-	 * @return true, if all FlowNodeInstances have the given state
-	 */
-	boolean checkFlowNodeInstanceState(String state, TProcess process, String processInstanceId) {
-		
-		String processId = IdService.getUniqueProcessId(client, process);
-		Set<FlowNodeInstance> fni = flowNodeInstanceRepo.findAllFlowNodeInstances(processId, processInstanceId);
-		
-		if (fni.size()==0) {
-			return false;
-		}
-		
-		for (FlowNodeInstance flowNodeInstance : fni) {
-			if (!flowNodeInstance.getNodeInstanceState().equals(state)) {
-				return false;
-			}
-		}
-		
-		return true;
-	}
-	
-	/**
-	 * Count the flow node instances in a given state.
-	 *
-	 * @param state the state
-	 * @return the int
-	 */
-	int countFlowNodeInstanceWithState(String state, TProcess process, String processInstanceId) {
-		String processId = IdService.getUniqueProcessId(client, process);
-		Set<FlowNodeInstance> fni = flowNodeInstanceRepo.findAllFlowNodeInstances(processId, processInstanceId);
-		
-		int counter = 0;
-		
-		for (FlowNodeInstance flowNodeInstance : fni) {
-			if (flowNodeInstance.getNodeInstanceState().equals(state)) {
-				counter++;
-			}
-		}
-		
-		return counter;
-	}
-	
-	/**
-	 * Count the flow node instances of a process.
-	 *
-	 * @param state the state
-	 * @return the int
-	 */
-	int countAllProcessInstances(TProcess process) {
-		String processId = IdService.getUniqueProcessId(client, process);
-		Set<String> fni = flowNodeInstanceRepo.findAllFlowNodeInstances(processId);
-
-		return fni.size();
-	}
-	
-
-	/**
-	 * Check node instance for a given state.
-	 *
-	 * @param process the jaxb process
-	 * @param id the id of the flow node to check
-	 * @param state the desired state
-	 */
-	private void checkNodeInstance(TProcess process, String id, String state) {
-		String flowNodeId = IdService.getUniqueFlowNodeId(client, process, null, id); // default throw
-		String processId = IdService.getUniqueProcessId(client, process);
-		FlowNodeInstance nodeInstance = flowNodeInstanceRepo.findFlowNodeInstance(processId, flowNodeId, defaultInstanceId);
-		assertNotNull(nodeInstance);
-		assertEquals(state, nodeInstance.getNodeInstanceState());
 	}
 }
